5c15bde914d860ab7a74aa1f59e72c0d689689c5
eibart <136814378+eibart@users.noreply.github.com>
2023-10-02 19:40:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var buttonR = document.getElementById("random");
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, "+ color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";";
+
+}
+// onLoad same as User defined
+window.onload = setGradient;
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+function generateColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var randomBG = "rgb(" + r + "," + g + "," + b + ")";
+    document.body.style.background = randomBG;
+    css.textContent = "RandomColorCode: " + body.style.background;
+
+    // oude code = geen variable
+    // let red, green, blue;
+    // red = Math.floor(Math.random() * 256);
+    // green = Math.floor(Math.random() * 256);
+    // blue = Math.floor(Math.random() * 256);
+  
+    // let color = `rgb(${red}, ${green}, ${blue})`;
+    // console.log(color);
+  }
+  
+
+buttonR.addEventListener("click", generateColor);
+
+
+
